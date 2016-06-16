78245a7f63effea1294c0071481706b3d1cef524
aoverholtzer <aoverholtzer@users.noreply.github.com>
2016-06-15 18:46:15 -0700

    Add Installation queries

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,31 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define("ios_install_count", function (request, response){
+    Parse.Cloud.useMasterKey();
+    var query = new Parse.Query("_Installation");
+    query.equalTo("deviceType","ios");
+    query.count({
+        success: function(count){
+            response.success(count);
+        },
+        error: function(error){
+            response.error(error);
+        }       
+    });
+});
+
+Parse.Cloud.define("android_install_count", function (request, response){
+    Parse.Cloud.useMasterKey();
+    var query = new Parse.Query("_Installation");
+    query.equalTo("deviceType","android");
+    query.count({
+        success: function(count){
+            response.success(count);
+        },
+        error: function(error){
+            response.error(error);
+        }       
+    });
+});
