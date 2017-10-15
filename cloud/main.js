a46baebcfbad162ebcd8808db18153ee07fbbfba
peteperl <peteperlegos@gmail.com>
2017-10-15 13:37:39 -0700

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,30 +1,3 @@
-
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
-});
-
-
-Parse.Cloud.beforeFind('KG2RobotModel', function(req) {
-  let isMaster = req.master; // if the query is run with masterKey
-  if (!isMaster) {
-    let query = req.query; // the Parse.Query
-
-    // limit query to 1 result
-    query.limit(1);
-
-    // validate uuid query constraint
-    let uuidType = typeof query._where.uuid;
-    if (uuidType == 'undefined') {
-      // uuid not specified or is part of a compound query: deny find
-      throw new Parse.Error(101, 'uuid not specified');
-    } else if (uuidType != 'string') {
-      // uuid not a string or is not part of an exact match query: deny find
-      throw new Parse.Error(102, 'uuid not a string');
-    }
-  }
-});
-
-
 Parse.Cloud.beforeSave("KG2RobotModel", function(req, res) {
   // validation constraints
   const MIN_DRIVING_DISTANCE_INCREMENT = 0;
@@ -35,10 +8,10 @@ Parse.Cloud.beforeSave("KG2RobotModel", function(req, res) {
   const MAX_NAME_LENGTH = 1000;
   const MAX_PROGRAM_INSTRUCTIONS = 1000;
 
-  // white list for Robot Names
+  // white list for robot names
   const ALLOWABLE_NAMES = ["Alpha", "Beta", "Gamma", "Delta"];
 
-  // white list for Robot Program Instructions
+  // white list for robot program instructions
   const ALLOWABLE_INSTRUCTIONS = ["lights_blue", "lights_green", "lights_purple", "lights_white"];
 
   let isMaster = req.master; // if the save is run with masterKey
@@ -178,33 +151,26 @@ Parse.Cloud.beforeDelete("KG2RobotModel", function(req, res) {
 });
 
 
+Parse.Cloud.beforeFind('KG2RobotModel', function(req) {
+  let isMaster = req.master; // if the query is run with masterKey
+  if (!isMaster) {
+    let query = req.query; // the Parse.Query
 
-Parse.Cloud.define("ios_install_count", function (request, response){
-    Parse.Cloud.useMasterKey();
-    var query = new Parse.Query(Parse.Installation);
-    query.equalTo("deviceType","ios");
-    query.count({
-        useMasterKey: true,
-        success: function(count){
-            response.success(count);
-        },
-        error: function(error){
-            response.error(error);
-        }
-    });
-});
+    // limit query to 1 result
+    query.limit(1);
 
-Parse.Cloud.define("android_install_count", function (request, response){
-    Parse.Cloud.useMasterKey();
-    var query = new Parse.Query("_Installation");
-    query.equalTo("deviceType","android");
-    query.count({
-        useMasterKey: true,
-        success: function(count){
-            response.success(count);
-        },
-        error: function(error){
-            response.error(error);
-        }
-    });
+    // validate uuid query constraint
+    let uuidType = typeof query._where.uuid;
+    if (uuidType == 'undefined') {
+      // uuid not specified or is part of a compound query
+      // deny find
+      throw new Parse.Error(101, 'uuid not specified');
+    } else if (uuidType != 'string') {
+      // uuid not a string or is not part of an exact match query
+      // deny find
+      throw new Parse.Error(102, 'uuid not a string');
+    }
+  }
+
+  // allow find
 });
